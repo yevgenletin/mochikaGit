9180a9b3b646f5f13e039f207d238cb756d75f73
yevge <yevgen.letin@gmail.com>
2022-05-28 13:23:10 +0200

    Add a message when everything is ok

--- gitCommit.py
+++ gitCommit.py
@@ -1,2 +1,18 @@
+import os
+import sys
+
+
 print("git commit -a -m")
 print("git log -p")
+
+def check_reboot():
+    """Return True if the computer has a pending reboot"""
+    return os.path.exist("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    print("Everything ok")
+    sys.exit(0)
+main()
